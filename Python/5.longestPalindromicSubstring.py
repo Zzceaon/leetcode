030a25e96c15594fa6f8fb12a976980fdb67efb2
YRASA <zzceaon@outlook.com>
2020-04-10 01:00:10 +0800

    add 5.longestPalindromicSubstring.py

---
+++ Python/5.longestPalindromicSubstring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        palindrome = ''
+        for i in range(len(s)):
+            len1 = len(self.getlongestPalindrome(s, i, i))
+            if len1 > len(palindrome):
+                palindrome = self.getlongestPalindrome(s, i, i)
+            len2 = len(self.getlongestPalindrome(s, i, i + 1))
+            if len2 > len(palindrome):
+                palindrome = self.getlongestPalindrome(s, i, i + 1)
+        return palindrome
+    def getlongestPalindrome(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l + 1: r]
