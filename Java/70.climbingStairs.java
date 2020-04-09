9f84b00f9eb445a1fe0fa52d6d10666e7cca810c
YRASA <zzceaon@outlook.com>
2020-04-10 03:42:13 +0800

    add 70.climbingStairs.java

---
+++ Java/70.climbingStairs.java
@@ -0,0 +1,34 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         double sqrt5 = Math.sqrt(5);
+//         double fibn = Math.pow((1 + sqrt5) / 2, n + 1) - Math.pow((1 - sqrt5) / 2, n + 1);
+//         return (int)(fibn/sqrt5);
+//     }    
+// }
+class Solution {
+    public int climbStairs(int n) {
+        int[][] q = {{1, 1}, {1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][0];
+    }
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
+}
