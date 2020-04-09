ba9c5893ed73a9f1327ae6de7c8bdc810137f92d
YRASA <zzceaon@outlook.com>
2020-04-10 03:42:57 +0800

    add 70.climbingStairs.js

---
+++ Javascript/70.climbingStairs.js
@@ -0,0 +1,9 @@
+var climbStairs = function(n) {
+    const memo = [];
+    memo[1] = 1;
+    memo[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        memo[i] = memo[i - 2] + memo[i - 1];
+    }
+    return memo[n];
+};
