80d9f10d04b3c736e0a534d134be7b66fa4f891d
YRASA <zzceaon@outlook.com>
2020-04-28 03:23:42 +0800

    modify Java/14. longestCommonPrefix.java

--- Java/14. longestCommonPrefix.java
+++ Java/14. longestCommonPrefix.java
@@ -1,3 +1,4 @@
+// 水平扫描
 // class Solution {
 //     public String longestCommonPrefix(String[] strs) {
 //         int n = strs.length;
@@ -20,7 +21,7 @@
 //         return sb.toString();
 //     }
 // }
-
+// 水平扫描
 // class Solution {
 //     public String longestCommonPrefix(String[] strs) {
 //         if (strs.length == 0) return "";
@@ -34,18 +35,76 @@
 //         return prefix;
 //     }
 // }
+// 分治
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         if (strs == null || strs.length == 0) return "";
+//         for (int i = 0; i < strs[0].length(); i++) {
+//             char c = strs[0].charAt(i);
+//             for (int j = 1; j < strs.length; j++) {
+//                 if (i == strs[j].length() || strs[j].charAt(i) != c) {
+//                     return strs[0].substring(0, i);
+//                 }
+//             }
+//         }
+//         return strs[0];
+//     }
+// }
 
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         if (strs == null || strs.length == 0) return "";
+//         return longestCommonPrefix(strs, 0, strs.length - 1);
+//     }
+//     private String longestCommonPrefix(String[] strs, int l, int r) {
+//         if (l == r) {
+//             return strs[l];
+//         }
+//         else {
+//             int mid = (l + r) / 2;
+//             String lcpLeft = longestCommonPrefix(strs, l, mid);
+//             String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+//             return commonPrefix(lcpLeft, lcpRight);
+//         }
+//     }
+//     String commonPrefix(String left, String right) {
+//         int min = Math.min(left.length(), right.length());
+//         for (int i = 0; i < min; i++) {
+//             if (left.charAt(i) != right.charAt(i)) {
+//                 return left.substring(0, i);
+//             }
+//         }
+//         return left.substring(0, min);
+//     }
+// }
+// 二分查找
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c) {
-                    return strs[0].substring(0, i);
-                }
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLen = Math.min(minLen, str.length());
+        }
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (isCommonPrefix(strs, middle)) {
+                low = middle + 1;
+            }
+            else {
+                high = middle -1;
             }
         }
-        return strs[0];
+        return strs[0].substring(0, (low + high) / 2);
+    }
+    private boolean isCommonPrefix(String[] strs, int len) {
+        String str1 = strs[0].substring(0, len);
+        for (int i =1; i < strs.length; i++) {
+            if (!strs[i].startsWith(str1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
