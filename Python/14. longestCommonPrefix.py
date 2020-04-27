fac1878e3c95597009ca7848549e2ae571ed82ba
YRASA <zzceaon@outlook.com>
2020-04-27 21:27:03 +0800

    add Python/14. longestCommonPrefix.py

---
+++ Python/14. longestCommonPrefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ""
+        i = 0
+        while True:
+            try:
+                sets = set(string[i] for string in strs)
+                if len(sets) == 1:
+                    result += sets.pop()
+                    i += 1
+                else:
+                    break
+            except Exception as e:
+                break
+        return result
