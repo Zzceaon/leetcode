1ca13e2e06a3d2664f04e259e12a753d3e1b6fb1
YRASA <zzceaon@outlook.com>
2020-04-29 05:07:23 +0800

    add Javascript/12.integerToRoman.js

---
+++ Javascript/12.integerToRoman.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const numbers = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let result = '';
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            num -= values[i];
+            result += numbers[i];
+        }
+    }
+    return result;
+};
