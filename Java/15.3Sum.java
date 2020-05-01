20f061a5ae4a9c3b12d550402b05ff88936d7a3b
YRASA <zzceaon@outlook.com>
2020-05-02 01:13:37 +0800

    add Java/15.3Sum.java

---
+++ Java/15.3Sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if (nums == null || len < 3) return ans;
+        Arrays.sort(nums);
+        for (int i = 0; i < len - 2; i++) {
+            if (nums[i] + nums[i + 1] + nums[i + 2]> 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if ( sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L + 1 < R && nums[L] == nums[L + 1]) L++;
+                    while (L < R - 1 && nums[R] == nums[R - 1]) R--;
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else  R--;
+            }
+        }
+        return ans; 
+    }
+}
