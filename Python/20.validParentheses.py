8c18827c8cb26af7e623d03e855b6c2c56e86b28
Zzceaon <zzceaon@outlook.com>
2020-05-08 21:17:14 +0800

    add Python/20.validParentheses.py

--- Python/20.validParentheses.py
+++ Python/20.validParentheses.py
@@ -1,3 +1,15 @@
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         mapping = {")": "(", "}": "{", "]": "["}
+#         for char in s:
+#             if char in mapping:
+#                 top_element = stack.pop() if stack else '#'
+#                 if top_element != mapping[char]:
+#                     return False
+#             else:
+#                 stack.append(char)
+#         return not stack
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
