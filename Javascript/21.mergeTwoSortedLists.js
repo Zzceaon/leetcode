7a3381da64fb55655a44d8b48983871e3aeb2a7e
Zzceaon <zzceaon@outlook.com>
2020-05-08 21:36:05 +0800

    add ../Javascript/21.mergeTwoSortedLists.js

---
+++ Javascript/21.mergeTwoSortedLists.js
@@ -0,0 +1,30 @@
+// 迭代
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    const prehead = new ListNode(-1);
+    let prev = prehead;
+    while (l1 != null && l2 != null) {
+        if (l1.val <= l2.val) {
+            prev.next = l1;
+            l1 = l1.next;
+        }
+        else {
+            prev.next = l2;
+            l2 = l2.next;
+        }
+        prev = prev.next;
+    }
+    prev.next = l1 === null ? l2 : l1;
+    return prehead.next;
+};
