d2d9a2ef96078417a2df61aa567face5a005adcf
Zzceaon <zzceaon@outlook.com>
2020-05-11 23:41:50 +0800

    add Java/22.generateParentheses.java

---
+++ Java/22.generateParentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        if (n == 0) {
+            return result;
+        }
+        helpler(n, n, "", result);
+        return result;
+    }
+    private void helpler(int l, int r, String item, List<String> result) {
+        if (r < l) {
+            return;
+        }
+        if (l == 0 && r == 0) {
+            result.add(item);
+        }
+        if (l > 0) {
+            helpler(l-1, r, item + '(', result);
+        }
+        if (r > 0) {
+            helpler(l, r-1, item + ')', result);
+        }
+    }
+}
