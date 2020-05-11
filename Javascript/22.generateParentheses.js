1c51166daeae9494bd0f5f1ce28f56f7a56f3b1d
Zzceaon <zzceaon@outlook.com>
2020-05-11 23:42:03 +0800

    add Javascript/22.generateParentheses.js

---
+++ Javascript/22.generateParentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    if (n === 0) {
+        return [];
+    }
+    let result = [];
+    helpler(n, n, '', result);
+    return result;
+};
+function helpler(l, r, item, result) {
+    if (r < l) {
+        return;
+    }
+    if (l ===0 && r === 0) {
+        result.push(item);
+    }
+    if (l > 0) {
+        helpler(l-1, r, item + '(', result);
+    }
+    if (r > 0) {
+        helpler(l, r-1, item + ')', result);
+    }
+}
