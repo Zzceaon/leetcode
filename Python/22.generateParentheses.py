86b5efd360a83d12f76bad5a6ef62d082c4e67a0
Zzceaon <zzceaon@outlook.com>
2020-05-14 02:09:49 +0800

    nodify Python/22.generateParentheses.py

--- Python/22.generateParentheses.py
+++ Python/22.generateParentheses.py
@@ -1,3 +1,22 @@
+# DFS
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         if n == 0:
+#             return[]
+#         result = []
+#         self.helpler(0, 0, n, '', result)
+#         return result
+#     def helpler(self, l, r, n, item, result):
+#         if l == n and r == n:
+#             result.append(item)
+#         if l < r:
+#             return
+#         if l < n:
+#             self.helpler(l+1, r, n, item + '(', result)
+#         if r < n:
+#             self.helpler(l, r+1, n, item + ')', result)
+
+# DFS
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         if n == 0:
