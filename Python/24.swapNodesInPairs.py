705a97da0ac82c4683880d18c4edf304031a96de
Zzceaon <zzceaon@outlook.com>
2020-05-17 07:35:47 +0800

    add Python/24.swapNodesInPairs.py

--- Python/24.swapNodesInPairs.py
+++ Python/24.swapNodesInPairs.py
@@ -14,3 +14,30 @@
 #             p.next = temp.next
 #             p = temp
 #         return new_start
+
+# 递归
+# class Solution:
+#     def swapPairs(self, head: ListNode) -> ListNode:
+#         if not head or not head.next:
+#             return head
+#         first_node = head
+#         second_node = head.next
+#         first_node.next = self.swapPairs(second_node.next)
+#         second_node.next = first_node
+#         return second_node
+
+# 迭代
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev_node = dummy
+        while head and head.next:
+            first_node = head
+            second_node = head.next
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+            prev_node = first_node
+            head = first_node.next
+        return dummy.next
