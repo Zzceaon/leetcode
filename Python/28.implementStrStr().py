e025e67017fef937ec6a62f00bdbfb04f0b7676c
Zzceaon <zzceaon@outlook.com>
2020-05-31 22:11:25 +0800

    28.implementStrStr().py

--- Python/28.implementStrStr().py
+++ Python/28.implementStrStr().py
@@ -5,22 +5,22 @@
 #                 return i
 #         return -1
 
-# 双指针
-# class Solution:
-#     def strStr(self, haystack: str, needle: str) -> int:
-#         L, n = len(needle), len(haystack)
-#         if L == 0:
-#             return 0
-#         pn = 0
-#         while pn < n - L + 1:
-#             while pn < n - L + 1 and haystack[pn] != needle[0]:
-#                 pn += 1
-#             curr_len = pL = 0
-#             while pL < L and pn < n and haystack[pn] == needle[pL]:
-#                 pn += 1
-#                 pL += 1
-#                 curr_len += 1
-#             if curr_len == L:
-#                 return pn - L
-#             pn = pn - curr_len + 1
-#         return -1
+双指针
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        L, n = len(needle), len(haystack)
+        if L == 0:
+            return 0
+        pn = 0
+        while pn < n - L + 1:
+            while pn < n - L + 1 and haystack[pn] != needle[0]:
+                pn += 1
+            curr_len = pL = 0
+            while pL < L and pn < n and haystack[pn] == needle[pL]:
+                pn += 1
+                pL += 1
+                curr_len += 1
+            if curr_len == L:
+                return pn - L
+            pn = pn - curr_len + 1
+        return -1
