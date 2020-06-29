e57316d2ffe81f4ffd4775d6e4a5c31dec9c9582
Zzceaon <zzceaon@outlook.com>
2020-06-29 21:15:03 +0800

    Python/102.binaryTreeLevelOrderTraversal.py

--- Python/102.binaryTreeLevelOrderTraversal.py
+++ Python/102.binaryTreeLevelOrderTraversal.py
@@ -19,3 +19,16 @@
 #       return res
 
 # DFS
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+      res = []
+      self.level(root, 0, res)
+      return res
+    def level(self, root, level, res):
+      if not root:  return
+      if len(res) == level: res.append([])
+      res[level].append(root.val)
+      if root.left:
+        self.level(root.left, level + 1, res)
+      if root.right:
+        self.level(root.right, level + 1, res)
