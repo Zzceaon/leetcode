f4536a9afb9654fd4c4add6506df3defae649363
Zzceaon <zzceaon@outlook.com>
2020-07-05 12:42:22 +0800

    Java/206.reverseLinkedList.java

--- Java/206.reverseLinkedList.java
+++ Java/206.reverseLinkedList.java
@@ -1,17 +1,17 @@
 // 迭代
-// class Solution {
-//     public ListNode reverseList(ListNode head) {
-//         ListNode prev = null;
-//         ListNode curr = head;
-//         while (curr != null) {
-//             ListNode nextTemp = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = nextTemp;
-//         }
-//         return prev;
-//     }
-// }
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
 
 // 递归
 class Solution {
