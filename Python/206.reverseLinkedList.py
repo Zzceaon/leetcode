073905c8dc0a6f47150e19bd9853de8a27466b14
Zzceaon <zzceaon@outlook.com>
2020-07-05 12:42:43 +0800

    Python/206.reverseLinkedList.py

--- Python/206.reverseLinkedList.py
+++ Python/206.reverseLinkedList.py
@@ -1,14 +1,14 @@
 # 迭代
-# class Solution:
-#     def reverseList(self, head: ListNode) -> ListNode:
-#         prev = None
-#         curr = head
-#         while(curr != None):
-#             nextTemp = curr.next
-#             curr.next = prev
-#             prev = curr
-#             curr = nextTemp
-#         return prev
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while(curr != None):
+            nextTemp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextTemp
+        return prev
 
 # 递归
 class Solution:
