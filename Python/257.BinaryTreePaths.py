be2c784c72e0a0e91e62c4fbd5202c0e48466a9d
Zzceaon <zzceaon@outlook.com>
2020-09-05 23:52:32 +0800

    257.BinaryTreePaths

--- Python/257.BinaryTreePaths.py
+++ Python/257.BinaryTreePaths.py
@@ -14,4 +14,24 @@ class Solution:
       construct_paths(root, '')
       return paths
 
-# BFS
+# BFS O(N^2) O(N^2)
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+      paths = list()
+      if not root:
+        return paths
+      node_queue = collections.deque([root])
+      path_queue = collections.deque([str(root.val)])
+      while node_queue:
+        node = node_queue.popleft()
+        path = path_queue.popleft()
+        if not node.left and not node.right:
+          paths.append(path)
+        else:
+          if node.left:
+            node_queue.append(node.left)
+            path_queue.append(path + '->' + str(node.left.val))
+          if node.right:
+            node_queue.append(node.right)
+            path_queue.append(path + '->' + str(node.right.val))
+      return paths
