0e1a5e9043572a882e5b9f438732f7906b147016
Zzceaon <zzceaon@outlook.com>
2020-09-07 11:38:26 +0800

    322.CoinChange

---
+++ Javascript/322.CoinChange.js
@@ -0,0 +1,13 @@
+var coinChange = function(coins, amount) {
+  const f = []
+  f[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    f[i] = Infinity
+    for (let j = 0; j < coins.length; j++) {
+      if (i - coins[j] >= 0) {
+        f[i] = Math.min(f[i], f[i - coins[j]] + 1)
+      }
+    }
+  }
+  return f[amount] === Infinity ? -1 : f[amount]
+}
