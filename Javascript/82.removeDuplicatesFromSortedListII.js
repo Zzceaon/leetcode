c4a9f4113ee7672590819932a4ff9a953e16d853
Zzceaon <zzceaon@outlook.com>
2021-03-26 00:40:50 +0800

    82.removeDuplicatesFromSortedListII

---
+++ Javascript/82.removeDuplicatesFromSortedListII.js
@@ -0,0 +1,18 @@
+var deleteDuplicates = function(head) {
+  if (!head) {
+    return head;
+  }
+  const dummy = new ListNode(0, head);
+  let cur = dummy;
+  while (cur.next && cur.next.next) {
+    if (cur.next.val == cur.next.next.val) {
+      const x = cur.next.val;
+      while (cur.next && cur.next.val == x) {
+        cur.next = cur.next.next;
+      }
+    }else {
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
+};
