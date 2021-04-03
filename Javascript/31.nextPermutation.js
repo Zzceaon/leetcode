6d47dc5eb51f11d85a072f58ba260f7fbe91108f
Zzceaon <zzceaon@outlook.com>
2021-04-03 15:16:33 +0800

    add 31.nextPermutation

---
+++ Javascript/31.nextPermutation.js
@@ -0,0 +1,23 @@
+// 时间复杂度：O(N)O(N)，其中 NN 为给定序列的长度。我们至多只需要扫描两次序列，以及进行一次反转操作。
+// 空间复杂度：O(1)O(1)，只需要常数的空间存放若干变量。
+const nextPermutation = (nums) => {
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (j >= 0 && nums[j] <= nums[i]) {
+      j--;
+    }
+    [nums[i], nums[j]] = [nums[j], nums[i]]
+  }
+  // 如果i等于-1, 说明是递减序列
+  let l = i + 1;
+  let r = nums.length - 1;
+  while (l < r) {
+    [nums[l], nums[r]] = [nums[r], nums[l]];
+    l++;
+    r--;
+  }
+};
