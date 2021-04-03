0b2b3a0330a6d5ac290ec38005609943d4899161
Zzceaon <zzceaon@outlook.com>
2021-04-03 16:41:22 +0800

    add 48.rotateImage

---
+++ Javascript/48.rotateImage.js
@@ -0,0 +1,15 @@
+// 时间复杂度：O(N^2)
+// 空间复杂度：O(1)为原地翻转得到的原地旋转。
+const rotate = matrix => {
+  const n = matrix.length;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = 0; j < n; j++) {
+      [matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
