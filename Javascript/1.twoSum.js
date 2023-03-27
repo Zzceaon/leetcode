d885af72bf7a90df15ebda63e1425a75ed15453f
Zzceaon <zzceaon@outlook.com>
2023-03-28 01:45:23 +0800

    test

--- Javascript/1.twoSum.js
+++ Javascript/1.twoSum.js
@@ -36,7 +36,7 @@ const twoSum = function(nums, target) {
         }
     }
 };
-// console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 9));
 
 // const twoSum = function(nums, target) {
 //     let obj = {};
